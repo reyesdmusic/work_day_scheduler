168c621e06c6601bb5e9dba22602f1f8f6b366c7
Danny Reyes <reyesdmusic@gmail.com>
2020-03-11 23:29:31 -0400

    worked on responsive layout and add clear button and functionality

--- script.js
+++ script.js
@@ -5,49 +5,66 @@ $(document).ready(function() {
     let m = moment().format("HH");
     console.log(m);
     
-    var themeSwitcher = document.querySelector("#time-box");
-    var container = document.querySelector("#text-box");
-    var otherContainer = document.querySelector("#user-edit");
-    var mode = "past";
-    
-    themeSwitcher.addEventListener("click", function() {
-      console.log("clicked");
-      if (mode === "past") {
-        mode = "future";
-        container.setAttribute("class", "future col-lg-8 col-md-6 col-sm-12 col-xs-12");
-        otherContainer.setAttribute("class", "future col-lg-8 col-md-6 col-sm-12 col-xs-12");
-      } else {
-        mode = "past";
-        container.setAttribute("class", "past col-lg-8 col-md-6 col-sm-12 col-xs-12");
-        otherContainer.setAttribute("class", "past col-lg-8 col-md-6 col-sm-12 col-xs-12");
-      }
-    });
 
     $("#save-box").hide();
     $("#cancel-box").hide();
-    $("#text-box").text("My shirona");
+    $("#set-text-9am").hide();
+    $("#user-edit-9am").hide();
+    $("#clear-box").hide();
+    
 
     $("#edit-box").on("click", function(){
         $("#edit-box").hide();
         $("#cancel-box").show();
         $("#save-box").show();
+        $("#text-box-9am").show();
+        $("#set-text-9am").hide();
+        $("#user-edit-9am").show();
+        $("#clear-box").show();
     })
 
     $("#cancel-box").on("click", function(){
         $("#edit-box").show();
         $("#cancel-box").hide();
         $("#save-box").hide();
+        $("#user-edit-9am").hide();
+        $("#text-box-9am").hide();
+        $("#set-text-9am").show();
+        $("#clear-box").hide();
     })
 
     $("#save-box").on("click", function(){
+        $("#set-text-9am").text($("#user-edit-9am").val());
+        $("#set-text-9am").show();
         $("#edit-box").show();
         $("#cancel-box").hide();
         $("#save-box").hide();
+        $("#text-box-9am").hide();
+        $("#clear-box").hide();
+    })
+       
+      $("#clear-box").on("click", function(){
+        $("#user-edit-9am").val("").empty();
     })
 
-
-
-
-
+    if (m > 9) {
+      $("#text-box-9am").attr("class", "past col-lg-8 col-md-12")
+      $("#set-text-9am").attr("class", "past col-lg-8 col-md-12")
+      $("#user-edit-9am").attr("class", "past-input")
+    }
+    
+    if (m === 9) {
+      $("#text-box-9am").attr("class", "now col-lg-8 col-md-12")
+      $("#set-text-9am").attr("class", "now col-lg-8 col-md-12")
+      $("#user-edit-9am").attr("class", "now col-lg-8 col-md-12")
+    }
+    
+    
+    if (m < 9) {
+      $("#text-box-9am").attr("class", "future col-lg-8 col-md-12")
+      $("#set-text-9am").attr("class", "future col-lg-8 col-md-12")
+      $("#user-edit-9am").attr("class", "future col-lg-8 col-md-12")
+    }
 
 })
+
