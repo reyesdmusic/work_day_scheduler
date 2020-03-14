8e143cd62efa7417b60dd572df7b89ebaef3573b
Danny Reyes <reyesdmusic@gmail.com>
2020-03-13 21:41:21 -0400

    have fully functioningapp based on assignment criterea. but i want to add a clear all button and edit style a bit

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
 
   render9am();
   render10am();
+  render11am();
+  render12pm();
+  render1pm();
+  render2pm();
+  render3pm();
+  render4pm();
+  render5pm();
 
     let m = moment().format("HH");
-    console.log(m);
+  
     
 
     $("#save-box-9am").hide();
@@ -21,6 +28,58 @@ $(document).ready(function() {
     $("#user-edit-10am").hide();
     $("#clear-box-10am").hide();
     $("#set-text-10am").show();
+
+    $("#save-box-11am").hide();
+    $("#cancel-box-11am").hide();
+    $("#text-box-11am").hide();
+    $("#user-edit-11am").hide();
+    $("#clear-box-11am").hide();
+    $("#set-text-11am").show();
+
+    $("#save-box-12pm").hide();
+    $("#cancel-box-12pm").hide();
+    $("#text-box-12pm").hide();
+    $("#user-edit-12pm").hide();
+    $("#clear-box-12pm").hide();
+    $("#set-text-12pm").show();
+
+    $("#save-box-1pm").hide();
+    $("#cancel-box-1pm").hide();
+    $("#text-box-1pm").hide();
+    $("#user-edit-1pm").hide();
+    $("#clear-box-1pm").hide();
+    $("#set-text-1pm").show();
+
+    $("#save-box-2pm").hide();
+    $("#cancel-box-2pm").hide();
+    $("#text-box-2pm").hide();
+    $("#user-edit-2pm").hide();
+    $("#clear-box-2pm").hide();
+    $("#set-text-2pm").show();
+
+    $("#save-box-3pm").hide();
+    $("#cancel-box-3pm").hide();
+    $("#text-box-3pm").hide();
+    $("#user-edit-3pm").hide();
+    $("#clear-box-3pm").hide();
+    $("#set-text-3pm").show();
+
+    $("#save-box-4pm").hide();
+    $("#cancel-box-4pm").hide();
+    $("#text-box-4pm").hide();
+    $("#user-edit-4pm").hide();
+    $("#clear-box-4pm").hide();
+    $("#set-text-4pm").show();
+
+    $("#save-box-5pm").hide();
+    $("#cancel-box-5pm").hide();
+    $("#text-box-5pm").hide();
+    $("#user-edit-5pm").hide();
+    $("#clear-box-5pm").hide();
+    $("#set-text-5pm").show();
+
+
+//////////////////////////////////
 
     $("#edit-box-9am").on("click", function(){
         $("#edit-box-9am").hide();
@@ -42,9 +101,7 @@ $(document).ready(function() {
         $("#clear-box-9am").hide();
     })
 
- 
-       
-      $("#clear-box-9am").on("click", function(){
+    $("#clear-box-9am").on("click", function(){
         $("#user-edit-9am").val("").empty();
     })
 
@@ -68,9 +125,6 @@ $(document).ready(function() {
     }
 
 
-
-
-
 var storedUserEdit9am = [];
 
 $("#save-box-9am").on("click", function(event) {
@@ -81,8 +135,6 @@ $("#save-box-9am").on("click", function(event) {
   localStorage.setItem('storedUserEdit9am', JSON.stringify(storedUserEdit9am));
 
 storedUserEdit9am.push(userEdit9am);
-
-console.log(storedUserEdit9am);
 
 localStorage.setItem('storedUserEdit9am', JSON.stringify(storedUserEdit9am));
 
@@ -103,7 +155,7 @@ function render9am() {
   var new9amText= JSON.parse(localStorage.getItem("storedUserEdit9am"));
 
   if (new9amText !== null) {
-  console.log(new9amText[0]);
+ 
 
 
   $("#set-text-9am").text(new9amText[0]);
@@ -111,9 +163,7 @@ function render9am() {
   }
 }
 
-
-
-
+///////////////////////////
 
 $("#edit-box-10am").on("click", function(){
     $("#edit-box-10am").hide();
@@ -134,10 +184,8 @@ $("#cancel-box-10am").on("click", function(){
     $("#set-text-10am").show();
     $("#clear-box-10am").hide();
 })
-
-
    
-  $("#clear-box-10am").on("click", function(){
+$("#clear-box-10am").on("click", function(){
     $("#user-edit-10am").val("").empty();
 })
 
@@ -193,24 +241,599 @@ var new10amText= JSON.parse(localStorage.getItem("storedUserEdit10am"));
 
 if (new10amText !== null) {
 
-console.log(new10amText[0]);
+ 
 $("#set-text-10am").text(new10amText[0]);
 
 }
 }
 
+/////////////////
+
+$("#edit-box-11am").on("click", function(){
+  $("#edit-box-11am").hide();
+  $("#cancel-box-11am").show();
+  $("#save-box-11am").show();
+  $("#text-box-11am").show();
+  $("#set-text-11am").hide();
+  $("#user-edit-11am").show();
+  $("#clear-box-11am").show();
+})
+
+$("#cancel-box-11am").on("click", function(){
+  $("#edit-box-11am").show();
+  $("#cancel-box-11am").hide();
+  $("#save-box-11am").hide();
+  $("#user-edit-11am").hide();
+  $("#text-box-11am").hide();
+  $("#set-text-11am").show();
+  $("#clear-box-11am").hide();
+})
+ 
+$("#clear-box-11am").on("click", function(){
+  $("#user-edit-11am").val("").empty();
+})
+
+if (m > 11) {
+$("#text-box-11am").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-11am").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-11am").attr("class", "past-input")
+}
+
+if (m === 11) {
+$("#text-box-11am").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-11am").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-11am").attr("class", "now-input")
+}
+
+
+if (m < 11) {
+$("#text-box-11am").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-11am").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-11am").attr("class", "future-input")
+}
+
+var storedUserEdit11am = [];
+
+$("#save-box-11am").on("click", function(event) {
+event.preventDefault();
+
+userEdit11am = $('#user-edit-11am').val();
+storedUserEdit11am = [];
+localStorage.setItem('storedUserEdit11am', JSON.stringify(storedUserEdit11am));
+
+storedUserEdit11am.push(userEdit11am);
+
+console.log(storedUserEdit11am);
+
+localStorage.setItem('storedUserEdit11am', JSON.stringify(storedUserEdit11am));
+
+
+$("#set-text-11am").show();
+$("#edit-box-11am").show();
+$("#cancel-box-11am").hide();
+$("#save-box-11am").hide();
+$("#text-box-11am").hide();
+$("#clear-box-11am").hide();
+
+render11am();
+
+});
+
+function render11am() {
+
+var new11amText= JSON.parse(localStorage.getItem("storedUserEdit11am"));
+
+if (new11amText !== null) {
+
+
+$("#set-text-11am").text(new11amText[0]);
+
+}
+}
+
+/////////////////
+
+$("#edit-box-12pm").on("click", function(){
+  $("#edit-box-12pm").hide();
+  $("#cancel-box-12pm").show();
+  $("#save-box-12pm").show();
+  $("#text-box-12pm").show();
+  $("#set-text-12pm").hide();
+  $("#user-edit-12pm").show();
+  $("#clear-box-12pm").show();
+})
+
+$("#cancel-box-12pm").on("click", function(){
+  $("#edit-box-12pm").show();
+  $("#cancel-box-12pm").hide();
+  $("#save-box-12pm").hide();
+  $("#user-edit-12pm").hide();
+  $("#text-box-12pm").hide();
+  $("#set-text-12pm").show();
+  $("#clear-box-12pm").hide();
+})
+ 
+$("#clear-box-12pm").on("click", function(){
+  $("#user-edit-12pm").val("").empty();
+})
+
+if (m > 12) {
+$("#text-box-12pm").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-12pm").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-12pm").attr("class", "past-input")
+}
+
+if (m === 12) {
+$("#text-box-12pm").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-12pm").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-12pm").attr("class", "now-input")
+}
+
+
+if (m < 12) {
+$("#text-box-12pm").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-12pm").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-12pm").attr("class", "future-input")
+}
+
+var storedUserEdit12pm = [];
+
+$("#save-box-12pm").on("click", function(event) {
+event.preventDefault();
+
+userEdit12pm = $('#user-edit-12pm').val();
+storedUserEdit12pm = [];
+localStorage.setItem('storedUserEdit12pm', JSON.stringify(storedUserEdit12pm));
+
+storedUserEdit12pm.push(userEdit12pm);
+
+console.log(storedUserEdit12pm);
+
+localStorage.setItem('storedUserEdit12pm', JSON.stringify(storedUserEdit12pm));
+
+
+$("#set-text-12pm").show();
+$("#edit-box-12pm").show();
+$("#cancel-box-12pm").hide();
+$("#save-box-12pm").hide();
+$("#text-box-12pm").hide();
+$("#clear-box-12pm").hide();
+
+render12pm();
+
+});
+
+function render12pm() {
+
+var new12pmText= JSON.parse(localStorage.getItem("storedUserEdit12pm"));
+
+if (new12pmText !== null) {
+
+
+$("#set-text-12pm").text(new12pmText[0]);
+
+}
+}
+
+/////////////////
+
+$("#edit-box-1pm").on("click", function(){
+  $("#edit-box-1pm").hide();
+  $("#cancel-box-1pm").show();
+  $("#save-box-1pm").show();
+  $("#text-box-1pm").show();
+  $("#set-text-1pm").hide();
+  $("#user-edit-1pm").show();
+  $("#clear-box-1pm").show();
+})
+
+$("#cancel-box-1pm").on("click", function(){
+  $("#edit-box-1pm").show();
+  $("#cancel-box-1pm").hide();
+  $("#save-box-1pm").hide();
+  $("#user-edit-1pm").hide();
+  $("#text-box-1pm").hide();
+  $("#set-text-1pm").show();
+  $("#clear-box-1pm").hide();
+})
+ 
+$("#clear-box-1pm").on("click", function(){
+  $("#user-edit-1pm").val("").empty();
+})
+
+if (m > 13) {
+$("#text-box-1pm").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-1pm").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-1pm").attr("class", "past-input")
+}
+
+if (m === 13) {
+$("#text-box-1pm").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-1pm").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-1pm").attr("class", "now-input")
+}
+
+
+if (m < 13) {
+$("#text-box-1pm").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-1pm").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-1pm").attr("class", "future-input")
+}
+
+var storedUserEdit1pm = [];
+
+$("#save-box-1pm").on("click", function(event) {
+event.preventDefault();
+
+userEdit1pm = $('#user-edit-1pm').val();
+storedUserEdit1pm = [];
+localStorage.setItem('storedUserEdit1pm', JSON.stringify(storedUserEdit1pm));
+
+storedUserEdit1pm.push(userEdit1pm);
+
+console.log(storedUserEdit1pm);
+
+localStorage.setItem('storedUserEdit1pm', JSON.stringify(storedUserEdit1pm));
+
+
+$("#set-text-1pm").show();
+$("#edit-box-1pm").show();
+$("#cancel-box-1pm").hide();
+$("#save-box-1pm").hide();
+$("#text-box-1pm").hide();
+$("#clear-box-1pm").hide();
+
+render1pm();
+
+});
+
+function render1pm() {
+
+var new1pmText= JSON.parse(localStorage.getItem("storedUserEdit1pm"));
+
+if (new1pmText !== null) {
+
+
+$("#set-text-1pm").text(new1pmText[0]);
+
+}
+}
+
+/////////////////
+
+
+$("#edit-box-2pm").on("click", function(){
+  $("#edit-box-2pm").hide();
+  $("#cancel-box-2pm").show();
+  $("#save-box-2pm").show();
+  $("#text-box-2pm").show();
+  $("#set-text-2pm").hide();
+  $("#user-edit-2pm").show();
+  $("#clear-box-2pm").show();
+})
+
+$("#cancel-box-2pm").on("click", function(){
+  $("#edit-box-2pm").show();
+  $("#cancel-box-2pm").hide();
+  $("#save-box-2pm").hide();
+  $("#user-edit-2pm").hide();
+  $("#text-box-2pm").hide();
+  $("#set-text-2pm").show();
+  $("#clear-box-2pm").hide();
+})
+ 
+$("#clear-box-2pm").on("click", function(){
+  $("#user-edit-2pm").val("").empty();
+})
+
+if (m > 14) {
+$("#text-box-2pm").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-2pm").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-2pm").attr("class", "past-input")
+}
+
+if (m === 14) {
+$("#text-box-2pm").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-2pm").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-2pm").attr("class", "now-input")
+}
+
+
+if (m < 14) {
+$("#text-box-2pm").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-2pm").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-2pm").attr("class", "future-input")
+}
+
+var storedUserEdit2pm = [];
+
+$("#save-box-2pm").on("click", function(event) {
+event.preventDefault();
+
+userEdit2pm = $('#user-edit-2pm').val();
+storedUserEdit2pm = [];
+localStorage.setItem('storedUserEdit2pm', JSON.stringify(storedUserEdit2pm));
+
+storedUserEdit2pm.push(userEdit2pm);
+
+console.log(storedUserEdit2pm);
+
+localStorage.setItem('storedUserEdit2pm', JSON.stringify(storedUserEdit2pm));
+
+
+$("#set-text-2pm").show();
+$("#edit-box-2pm").show();
+$("#cancel-box-2pm").hide();
+$("#save-box-2pm").hide();
+$("#text-box-2pm").hide();
+$("#clear-box-2pm").hide();
+
+render2pm();
+
+});
+
+function render2pm() {
+
+var new2pmText= JSON.parse(localStorage.getItem("storedUserEdit2pm"));
+
+if (new2pmText !== null) {
+
+
+$("#set-text-2pm").text(new2pmText[0]);
+
+}
+}
+
+/////////////////
 
 
 
+$("#edit-box-3pm").on("click", function(){
+  $("#edit-box-3pm").hide();
+  $("#cancel-box-3pm").show();
+  $("#save-box-3pm").show();
+  $("#text-box-3pm").show();
+  $("#set-text-3pm").hide();
+  $("#user-edit-3pm").show();
+  $("#clear-box-3pm").show();
+})
+
+$("#cancel-box-3pm").on("click", function(){
+  $("#edit-box-3pm").show();
+  $("#cancel-box-3pm").hide();
+  $("#save-box-3pm").hide();
+  $("#user-edit-3pm").hide();
+  $("#text-box-3pm").hide();
+  $("#set-text-3pm").show();
+  $("#clear-box-3pm").hide();
+})
+ 
+$("#clear-box-3pm").on("click", function(){
+  $("#user-edit-3pm").val("").empty();
+})
+
+if (m > 15) {
+$("#text-box-3pm").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-3pm").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-3pm").attr("class", "past-input")
+}
+
+if (m === 15) {
+$("#text-box-3pm").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-3pm").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-3pm").attr("class", "now-input")
+}
 
 
+if (m < 15) {
+$("#text-box-3pm").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-3pm").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-3pm").attr("class", "future-input")
+}
+
+var storedUserEdit3pm = [];
+
+$("#save-box-3pm").on("click", function(event) {
+event.preventDefault();
+
+userEdit3pm = $('#user-edit-3pm').val();
+storedUserEdit3pm = [];
+localStorage.setItem('storedUserEdit3pm', JSON.stringify(storedUserEdit3pm));
+
+storedUserEdit3pm.push(userEdit3pm);
+
+console.log(storedUserEdit3pm);
+
+localStorage.setItem('storedUserEdit3pm', JSON.stringify(storedUserEdit3pm));
 
 
+$("#set-text-3pm").show();
+$("#edit-box-3pm").show();
+$("#cancel-box-3pm").hide();
+$("#save-box-3pm").hide();
+$("#text-box-3pm").hide();
+$("#clear-box-3pm").hide();
+
+render3pm();
+
+});
+
+function render3pm() {
+
+var new3pmText= JSON.parse(localStorage.getItem("storedUserEdit3pm"));
+
+if (new3pmText !== null) {
 
 
+$("#set-text-3pm").text(new3pmText[0]);
+
+}
+}
+
+/////////////////
 
 
+$("#edit-box-4pm").on("click", function(){
+  $("#edit-box-4pm").hide();
+  $("#cancel-box-4pm").show();
+  $("#save-box-4pm").show();
+  $("#text-box-4pm").show();
+  $("#set-text-4pm").hide();
+  $("#user-edit-4pm").show();
+  $("#clear-box-4pm").show();
+})
 
+$("#cancel-box-4pm").on("click", function(){
+  $("#edit-box-4pm").show();
+  $("#cancel-box-4pm").hide();
+  $("#save-box-4pm").hide();
+  $("#user-edit-4pm").hide();
+  $("#text-box-4pm").hide();
+  $("#set-text-4pm").show();
+  $("#clear-box-4pm").hide();
+})
+ 
+$("#clear-box-4pm").on("click", function(){
+  $("#user-edit-4pm").val("").empty();
+})
+
+if (m > 16) {
+$("#text-box-4pm").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-4pm").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-4pm").attr("class", "past-input")
+}
+
+if (m === 16) {
+$("#text-box-4pm").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-4pm").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-4pm").attr("class", "now-input")
+}
+
+
+if (m < 16) {
+$("#text-box-4pm").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-4pm").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-4pm").attr("class", "future-input")
+}
+
+var storedUserEdit4pm = [];
+
+$("#save-box-4pm").on("click", function(event) {
+event.preventDefault();
+
+userEdit4pm = $('#user-edit-4pm').val();
+storedUserEdit4pm = [];
+localStorage.setItem('storedUserEdit4pm', JSON.stringify(storedUserEdit4pm));
+
+storedUserEdit4pm.push(userEdit4pm);
+
+localStorage.setItem('storedUserEdit4pm', JSON.stringify(storedUserEdit4pm));
+
+
+$("#set-text-4pm").show();
+$("#edit-box-4pm").show();
+$("#cancel-box-4pm").hide();
+$("#save-box-4pm").hide();
+$("#text-box-4pm").hide();
+$("#clear-box-4pm").hide();
+
+render4pm();
+
+});
+
+function render4pm() {
+
+var new4pmText= JSON.parse(localStorage.getItem("storedUserEdit4pm"));
+
+if (new4pmText !== null) {
+
+
+$("#set-text-4pm").text(new4pmText[0]);
+
+}
+}
+
+/////////////////
+
+$("#edit-box-5pm").on("click", function(){
+  $("#edit-box-5pm").hide();
+  $("#cancel-box-5pm").show();
+  $("#save-box-5pm").show();
+  $("#text-box-5pm").show();
+  $("#set-text-5pm").hide();
+  $("#user-edit-5pm").show();
+  $("#clear-box-5pm").show();
+})
+
+$("#cancel-box-5pm").on("click", function(){
+  $("#edit-box-5pm").show();
+  $("#cancel-box-5pm").hide();
+  $("#save-box-5pm").hide();
+  $("#user-edit-5pm").hide();
+  $("#text-box-5pm").hide();
+  $("#set-text-5pm").show();
+  $("#clear-box-5pm").hide();
+})
+ 
+$("#clear-box-5pm").on("click", function(){
+  $("#user-edit-5pm").val("").empty();
+})
+
+if (m > 16) {
+$("#text-box-5pm").attr("class", "past col-lg-8 col-md-12")
+$("#set-text-5pm").attr("class", "past col-lg-8 col-md-12")
+$("#user-edit-5pm").attr("class", "past-input")
+}
+
+if (m === 16) {
+$("#text-box-5pm").attr("class", "now col-lg-8 col-md-12")
+$("#set-text-5pm").attr("class", "now col-lg-8 col-md-12")
+$("#user-edit-5pm").attr("class", "now-input")
+}
+
+
+if (m < 16) {
+$("#text-box-5pm").attr("class", "future col-lg-8 col-md-12")
+$("#set-text-5pm").attr("class", "future col-lg-8 col-md-12")
+$("#user-edit-5pm").attr("class", "future-input")
+}
+
+var storedUserEdit5pm = [];
+
+$("#save-box-5pm").on("click", function(event) {
+event.preventDefault();
+
+userEdit5pm = $('#user-edit-5pm').val();
+storedUserEdit5pm = [];
+localStorage.setItem('storedUserEdit5pm', JSON.stringify(storedUserEdit5pm));
+
+storedUserEdit5pm.push(userEdit5pm);
+
+localStorage.setItem('storedUserEdit5pm', JSON.stringify(storedUserEdit5pm));
+
+
+$("#set-text-5pm").show();
+$("#edit-box-5pm").show();
+$("#cancel-box-5pm").hide();
+$("#save-box-5pm").hide();
+$("#text-box-5pm").hide();
+$("#clear-box-5pm").hide();
+
+render5pm();
+
+});
+
+function render5pm() {
+
+var new5pmText= JSON.parse(localStorage.getItem("storedUserEdit5pm"));
+
+if (new5pmText !== null) {
+
+
+$("#set-text-5pm").text(new5pmText[0]);
+
+}
+}
 
 })
 
